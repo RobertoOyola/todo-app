efa8b23aa4f8e42af9b3cc9bc8bf36b6916c204f
Roberto Oyola Vera <roberto.oyola@tecnologicoliceocristiano.edu.ec>
2025-04-30 21:23:39 -0500

    Coneccion con la base de datos aggregamos el insert y el completar tareas

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import routerTODOS from "./routes/todo.routes.js";
+
 const app = express();
+app.use(express.json());
 app.use(routerTODOS);
 
 app.listen(3000, () => console.log("Server is running on port ",3000));
--- src/routes/todo.routes.js
+++ src/routes/todo.routes.js
@@ -1,24 +1,17 @@
 import {TODOS} from "../constantes/constantes.js";
 import {Router} from "express";
+import { getTodos, getTodoById, CreateTodo, completeTodo, deleteTodo } from "../controllers/todo.controller.js";
 
 const routerTODOS = Router();
 
-routerTODOS.get('/todos', async (req, res) => {
-    res.status(200).json(TODOS);
-})
+routerTODOS.get('/todos', getTodos);
 
-routerTODOS.get('/todos/:id', async (req, res) => {
-    const { id } = req.params;
-    const resultadoBusqueda = TODOS.filter( x => x.id == id);
+routerTODOS.get('/todos/:id', getTodoById);
 
-    if(resultadoBusqueda.length > 0){
-        res.status(200).json(resultadoBusqueda[0]);
-    }else {
-        res.status(404).json({
-            mensaje: `No se encontro producto con id ${id}`
-        });
-    }
+routerTODOS.post('/todos', CreateTodo);
 
-})
+routerTODOS.post('/completeTodos/:id', completeTodo);
+
+routerTODOS.delete('/deleteTodos/:id', deleteTodo);
 
 export default routerTODOS;
